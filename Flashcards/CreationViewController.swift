623f38e63d5f6b6fcfb2975a6a1f0302fe959886
John Jakobsen <go4johne@gmail.com>
2020-02-29 18:19:30 -0500

    Lab 2 - Completed (Required + Optionals)

---
+++ Flashcards/CreationViewController.swift
@@ -0,0 +1,64 @@
+//
+//  CreationViewController.swift
+//  Flashcards
+//
+//  Created by Jenafer Morton on 2/29/20.
+//  Copyright © 2020 John Jakobsen. All rights reserved.
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+    
+    var flashcardsController: ViewController!
+    @IBOutlet weak var QuestionInput: UITextField!
+    @IBOutlet weak var Answer1: UITextField!
+    @IBOutlet weak var Answer3: UITextField!
+    @IBOutlet weak var Answer2: UITextField!
+    var initialQuestion: String?
+    var initialAnswer1: String?
+    var initialAnswer2: String?
+    var initialAnswer3: String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        QuestionInput.text = initialQuestion
+        Answer1.text = initialAnswer1
+        Answer2.text = initialAnswer2
+        Answer3.text = initialAnswer3
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapOnCancel(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func didTapOnDone(_ sender: Any) {
+        let questionText = QuestionInput.text
+        let Answer1Text = Answer1.text
+        let Answer2Text = Answer2.text
+        let Answer3Text = Answer3.text
+        
+        if questionText == nil || Answer1Text == nil || Answer2Text == nil || Answer3Text == nil || questionText == "" || Answer1Text == "" || Answer2Text == "" || Answer3Text == "" {
+            let alert = UIAlertController(title: "Missing text", message: "Please enter all fields", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default)
+            alert.addAction(okAction)
+            present(alert, animated: true)
+        } else {
+        
+        flashcardsController.updateFlashcard(question: questionText!, answer1: Answer1Text!, answer2: Answer2Text!, answer3: Answer3Text!)
+        
+        dismiss(animated: true)
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -42,14 +42,35 @@ class ViewController: UIViewController {
         bottomAnswer.layer.borderColor = #colorLiteral(red: 0.8273518778, green: 0.8797557677, blue: 0.9898599417, alpha: 1)
         bottomAnswer.layer.cornerRadius = 20.0
         
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
+        let navigationController = segue.destination as! UINavigationController
+        
+        let creationController = navigationController.topViewController as! CreationViewController
+        
+        creationController.flashcardsController = self
+        if segue.identifier == "EditSegue" {
+            creationController.initialQuestion = Question.text
+            creationController.initialAnswer1 = topAnswer.currentTitle
+            creationController.initialAnswer2 = middleAnswer.currentTitle
+            creationController.initialAnswer3 = bottomAnswer.currentTitle
+        }
+    }
+    
+    func updateFlashcard(question: String, answer1: String, answer2: String!, answer3: String!) {
+        Question.text = question
+        Answer.text = answer3
+        topAnswer.setTitle(answer1, for: .normal)
+        middleAnswer.setTitle(answer2, for: .normal)
+        bottomAnswer.setTitle(answer3, for: .normal)
     }
 
-    @IBAction func didTapFlashcard(_ sender: Any) {
+    @IBAction func didTapCardContainer(_ sender: Any) {
         Question.isHidden = !Question.isHidden
         topAnswer.layer.isHidden = false
         middleAnswer.layer.isHidden = false
-
     }
     
     @IBAction func didTapTop(_ sender: Any) {
@@ -60,6 +81,8 @@ class ViewController: UIViewController {
     }
     @IBAction func didTapBottom(_ sender: Any) {
         Question.layer.isHidden = true
+        topAnswer.layer.isHidden = true
+        middleAnswer.layer.isHidden = true
     }
 }
 
